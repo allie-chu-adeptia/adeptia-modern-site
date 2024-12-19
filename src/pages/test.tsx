d203495b3d296d02935ad50bdbef4bae57b521ca
allie-chu-adeptia <allie.chu@adeptia.com>
2024-12-19 12:44:07 -0800

    Apollo Client setup and WPGraphQL setup

---
+++ src/pages/test.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+type Post = {
+  excerpt: string;
+  uri: string;
+};
+
+export default function TestPage({ posts }: { posts: Post[] }) {
+
+    console.log('posts', posts);
+    return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        {posts.map(post => {
+          return (
+            <div key={post.uri}>
+              <h1>{post.excerpt}</h1>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+
+export async function getStaticProps() {
+    const client = new ApolloClient({
+      uri: 'https://www.staging14.adeptia.com/graphql',
+      cache: new InMemoryCache(),
+    });
+  
+    const response = await client.query({
+      query: gql`
+        query NewQuery {
+          posts {
+            edges {
+              node {
+                excerpt
+                uri
+              }
+            }
+          }
+        }
+      `,
+    });
+  
+    const posts = response.data.posts.edges.map(({ node }: { node: Post }) => node);
+
+    console.log('posts', posts);
+  
+    return {
+      props: {
+        posts: posts,
+      }
+    }
+  }
