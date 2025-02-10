17c16d84a63b87dd2950dfdddd9197315d4a3f86
allie-chu-adeptia <allie.chu@adeptia.com>
2025-02-10 13:37:30 -0500

    fix category chip path

--- src/lib/categoryChip.tsx
+++ src/lib/categoryChip.tsx
@@ -4,12 +4,13 @@ import { ExpandedCategory } from "@/sanity/types/local.types"
 export function CategoryChip({ categories, pathName }: { categories?: ExpandedCategory[], pathName: string }) {
 
     return (
+        console.log("category pathname", pathName),
         Array.isArray(categories) && (
             <div className="flex flex-wrap gap-2">
                 {categories.map((category: ExpandedCategory) => (
                     <Link
                         key={category._id}
-                        href={`/${pathName}?category=${category.slug}`}
+                        href={`?category=${category.slug}`}
                         className="rounded-full border border-dotted border-gray-300 bg-gray-50 px-2 text-sm/6 font-medium text-gray-500"
                     >
                         {category.name}
