bd1e2b2cb6b0d778a94affe14ac291cb82733725
allie-chu-adeptia <allie.chu@adeptia.com>
2025-03-13 12:36:47 -0400

    Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { NavbarNew } from "@/components/navbar-new";
 import { Footer } from "@/components/footer";
 import { SanityLive } from "@/sanity/lib/live";
 import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script"
 
 
 const geistSans = Geist({
@@ -33,9 +34,26 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TGBWNG');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGBWNG" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NavbarNew />
         {children}
         <SanityLive />
