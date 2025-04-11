889d22a1be1259d8d9f538f42a6356da6cb0ff19
allie-chu-adeptia <allie.chu@adeptia.com>
2025-04-11 11:01:39 -0400

    Attempting to add cirlce

---
+++ src/app/animations/page.tsx
@@ -0,0 +1,9 @@
+import { PlatformDiagram } from "@/animations/platformDiagram";
+
+export default function AnimationsPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <PlatformDiagram />
+        </div>
+    )
+}
