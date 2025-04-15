32d1c0f62c21e44d13ae7909df0b129d37f8ffb8
allie-chu-adeptia <allie.chu@adeptia.com>
2025-04-15 15:03:19 -0400

    Change banner text

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -14,7 +14,7 @@ export default function Banner() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="2" height="2" fill="none" viewBox="0 0 2 2">
                         <circle cx="1" cy="1" r="1" fill="#fff" />
                     </svg>
-                    <span>Schedule Meeting at the Conference</span>
+                    <span>Learn More</span>
                     <span aria-hidden="true">&rarr;</span>
                 </ClientLink>
             </p>
