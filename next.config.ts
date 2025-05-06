a931160af66a0fab7b815f06b18502dd396b31d0
allie-chu-adeptia <allie.chu@adeptia.com>
2025-05-06 06:42:07 -0700

    add new read token

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 import { createClient } from "@sanity/client";
+const { NEXT_PUBLIC_SANITY_PROJECT_ID, NEXT_PUBLIC_SANITY_DATASET, NEXT_PUBLIC_SANITY_API_VERSION } = process.env
 
 const client = createClient({
-  projectId: "5ujtwa6a",
-  dataset: "production",
+  projectId: NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: NEXT_PUBLIC_SANITY_DATASET,
   useCdn: false,
-  apiVersion: "2025-02-26"
+  apiVersion: NEXT_PUBLIC_SANITY_API_VERSION
 });
 
 const nextConfig: NextConfig = {
@@ -21,23 +22,6 @@ const nextConfig: NextConfig = {
   },
   async headers() {
     return [
-      // Content Security Policy
-      // {
-      //   source: "/(.*)", // Apply to all routes
-      //   headers: [
-      //     {
-      //       key: "Content-Security-Policy",
-      //       value:
-      //         "default-src 'self'; " +
-      //         "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://va.vercel-scripts.com/; " +
-      //         "connect-src 'self' 'unsafe-inline' https://5ujtwa6a.api.sanity.io/;" +
-      //         "style-src 'self' 'unsafe-inline'; " +
-      //         "font-src 'self' data:; " +
-      //         "img-src 'self' data: https://5ujtwa6a.api.sanity.io/ https://cdn.sanity.io/images/5ujtwa6a/production/;"
-      //     },
-      //   ],
-      // },
-      // X-Content-Type-Options 
       {
         source: "/(.*)",
         headers: [
